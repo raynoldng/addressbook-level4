ed18ca27fffd16d761b5003d8ca140a2314f2421
Jiang Yue <jiangyue12392@gmail.com>
2018-03-04 23:48:36 +0800

    implement the backup method

--- src/main/java/seedu/address/storage/XmlAddressBookStorage.java
+++ src/main/java/seedu/address/storage/XmlAddressBookStorage.java
@@ -23,8 +23,11 @@ public class XmlAddressBookStorage implements AddressBookStorage {
 
     private String filePath;
 
+    private String backupFilePath;
+
     public XmlAddressBookStorage(String filePath) {
         this.filePath = filePath;
+        this.backupFilePath = filePath + ".backup";
     }
 
     public String getAddressBookFilePath() {
@@ -79,4 +82,8 @@ public class XmlAddressBookStorage implements AddressBookStorage {
         XmlFileStorage.saveDataToFile(file, new XmlSerializableAddressBook(addressBook));
     }
 
+    @Override
+    public void backupAddressBook(ReadOnlyAddressBook addressBook) throws IOException {
+        saveAddressBook(addressBook, backupFilePath);
+    }
 }
