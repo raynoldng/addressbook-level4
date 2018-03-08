e779e9521f8c2b6d980abf912a2c690d95d373a3
william6364 <ganweiliang@gmail.com>
2018-03-08 23:19:58 +0800

    Added EpicEventUtil for testing

--- src/test/java/seedu/address/testutil/EpicEventBuilder.java
+++ src/test/java/seedu/address/testutil/EpicEventBuilder.java
@@ -4,11 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import seedu.address.model.event.EpicEvent;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
 import seedu.address.model.Name;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.util.SampleDataUtil;
 
---
+++ src/test/java/seedu/address/testutil/EpicEventUtil.java
@@ -0,0 +1,32 @@
+package seedu.address.testutil;
+
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+import seedu.address.logic.commands.AddEventCommand;
+import seedu.address.model.event.EpicEvent;
+
+/**
+ * A utility class for EpicEvent.
+ */
+public class EpicEventUtil {
+
+    /**
+     * Returns an add command string for adding the {@code event}.
+     */
+    public static String getAddEventCommand(EpicEvent event) {
+        return AddEventCommand.COMMAND_WORD + " " + getEventDetails(event);
+    }
+
+    /**
+     * Returns the part of command string for the given {@code event}'s details.
+     */
+    public static String getEventDetails(EpicEvent event) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(PREFIX_NAME + event.getName().name + " ");
+        event.getTags().stream().forEach(
+                s -> sb.append(PREFIX_TAG + s.tagName + " ")
+        );
+        return sb.toString();
+    }
+}
