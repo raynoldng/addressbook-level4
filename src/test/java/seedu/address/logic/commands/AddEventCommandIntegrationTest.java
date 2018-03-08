d041c2603e8dad5579707d8eb3488886b2105ead
william6364 <ganweiliang@gmail.com>
2018-03-08 23:43:38 +0800

    Added AddEventCommandIntegrationTest and added test to test creation of new event in model

---
+++ src/test/java/seedu/address/logic/commands/AddEventCommandIntegrationTest.java
@@ -0,0 +1,46 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.UndoRedoStack;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.event.EpicEvent;
+import seedu.address.testutil.EpicEventBuilder;
+
+public class AddEventCommandIntegrationTest {
+
+    private Model model;
+
+    @Before
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_newEvent_success() throws Exception {
+        EpicEvent validEvent = new EpicEventBuilder().build();
+
+        Model expectedModel = new ModelManager(model.getEventPlanner(), new UserPrefs());
+        expectedModel.addEvent(validEvent);
+
+        assertCommandSuccess(prepareCommand(validEvent, model), model,
+                String.format(AddEventCommand.MESSAGE_SUCCESS, validEvent), expectedModel);
+    }
+
+    /**
+     * Generates a new {@code AddEventCommand} which upon execution, adds {@code event} into the {@code model}.
+     */
+    private AddEventCommand prepareCommand(EpicEvent event, Model model) {
+        AddEventCommand command = new AddEventCommand(event);
+        command.setData(model, new CommandHistory(), new UndoRedoStack());
+        return command;
+    }
+}
