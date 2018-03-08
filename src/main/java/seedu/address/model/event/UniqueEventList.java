a74ae1cd0f1693973003678a037dc616db200840
william6364 <ganweiliang@gmail.com>
2018-03-08 21:46:57 +0800

    Added contains and add methods to UniqueEventList

--- src/main/java/seedu/address/model/event/UniqueEventList.java
+++ src/main/java/seedu/address/model/event/UniqueEventList.java
@@ -8,10 +8,33 @@ import java.util.List;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import seedu.address.model.event.exceptions.DuplicateEventException;
 
 public class UniqueEventList {
 
     private final ObservableList<EpicEvent> internalList = FXCollections.observableArrayList();
+
+    /**
+     * Returns true if the list contains an equivalent event as the given argument.
+     */
+    public boolean contains(EpicEvent toCheck) {
+        requireNonNull(toCheck);
+        return internalList.contains(toCheck);
+    }
+
+    /**
+     * Adds an event to the list.
+     *
+     * @throws DuplicateEventException if the event to add is a duplicate of an existing event in the list.
+     */
+    public void add(EpicEvent toAdd) throws DuplicateEventException {
+        requireNonNull(toAdd);
+        if (contains(toAdd)) {
+            throw new DuplicateEventException();
+        }
+        internalList.add(toAdd);
+    }
+
 
     @Override
     public boolean equals(Object other) {
