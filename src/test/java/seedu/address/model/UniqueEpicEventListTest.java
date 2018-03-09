367d65fbf869a9e9abc9195081c5085c9b21b92d
raynoldng <raynold.ng24@gmail.com>
2018-03-09 23:58:06 +0800

    Refactor Event to EpicEvent

--- src/test/java/seedu/address/model/UniqueEpicEventListTest.java
+++ src/test/java/seedu/address/model/UniqueEpicEventListTest.java
@@ -4,16 +4,16 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import seedu.address.model.event.UniqueEventList;
+import seedu.address.model.event.UniqueEpicEventList;
 
-public class UniqueEventListTest {
+public class UniqueEpicEventListTest {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
     @Test
     public void asObservableList_modifyList_throwsUnsupportedOperationException() {
-        UniqueEventList uniqueEventList = new UniqueEventList();
+        UniqueEpicEventList uniqueEpicEventList = new UniqueEpicEventList();
         thrown.expect(UnsupportedOperationException.class);
-        uniqueEventList.asObservableList().remove(0);
+        uniqueEpicEventList.asObservableList().remove(0);
     }
 }
