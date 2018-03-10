9f1f137b3f269e5723b98846020cd4326fbec1b1
jiangyue12392 <jiangyue12392@gmail.com>
2018-03-10 21:43:43 +0800

    change findEvent message display

--- src/main/java/seedu/address/logic/commands/FindEventCommand.java
+++ src/main/java/seedu/address/logic/commands/FindEventCommand.java
@@ -24,7 +24,7 @@ public class FindEventCommand extends Command {
     @Override
     public CommandResult execute() {
         model.updateFilteredEventList(predicate);
-        return new CommandResult(getMessageForPersonListShownSummary(model.getFilteredPersonList().size()));
+        return new CommandResult(getMessageForEventListShownSummary(model.getFilteredEventList().size()));
     }
 
     @Override
