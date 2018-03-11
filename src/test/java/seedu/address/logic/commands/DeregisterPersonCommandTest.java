5febe3844b24600c8cb24b782caffa885170e945
bayweiheng <bayweiheng@gmail.com>
2018-03-11 18:25:58 +0800

    update user guide for deregister command, fix style issues

--- src/test/java/seedu/address/logic/commands/DeregisterPersonCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeregisterPersonCommandTest.java
@@ -106,4 +106,5 @@ public class DeregisterPersonCommandTest {
 
         assertTrue(model.getFilteredPersonList().isEmpty());
     }
+    
 }
--- src/test/java/seedu/address/logic/commands/RegisterPersonCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RegisterPersonCommandTest.java
@@ -121,4 +121,5 @@ public class RegisterPersonCommandTest {
 
         assertTrue(model.getFilteredPersonList().isEmpty());
     }
+    
 }
