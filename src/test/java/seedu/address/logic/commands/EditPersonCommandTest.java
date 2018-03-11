d70a80c551a9397177c9e0a8f2daddf0d58addb5
bayweiheng <bayweiheng@gmail.com>
2018-03-12 05:35:23 +0800

    fix checkstyle issues

--- src/test/java/seedu/address/logic/commands/EditPersonCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditPersonCommandTest.java
@@ -1,7 +1,7 @@
 package seedu.address.logic.commands;
 
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
+//import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.DESC_BOB;
@@ -10,8 +10,8 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_PERSON_PHONE_BO
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PERSON_TAG_HUSBAND;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.CommandTestUtil.prepareRedoCommand;
-import static seedu.address.logic.commands.CommandTestUtil.prepareUndoCommand;
+//import static seedu.address.logic.commands.CommandTestUtil.prepareRedoCommand;
+//import static seedu.address.logic.commands.CommandTestUtil.prepareUndoCommand;
 import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
