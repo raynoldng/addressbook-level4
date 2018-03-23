f1a4c888ec7126924125e0fbf0d3dced35b5774f
william6364 <ganweiliang@gmail.com>
2018-03-23 13:03:47 +0800

    Added Attendance class

---
+++ src/main/java/seedu/address/model/attendance/Attendance.java
@@ -0,0 +1,8 @@
+package seedu.address.model.attendance;
+
+/**
+ * Represents the attendance of a person to an event in the event planner.
+ * Guarantees: person is immutable and not null
+ */
+public class Attendance {
+}
