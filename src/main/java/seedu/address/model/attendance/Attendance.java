6b9d52832e0c2019c3b6517c851d957d864056b3
william6364 <ganweiliang@gmail.com>
2018-03-23 16:15:58 +0800

    implemented Attendance class

--- src/main/java/seedu/address/model/attendance/Attendance.java
+++ src/main/java/seedu/address/model/attendance/Attendance.java
@@ -1,8 +1,68 @@
 package seedu.address.model.attendance;
 
+import java.util.Objects;
+
+import seedu.address.model.person.Person;
+
 /**
  * Represents the attendance of a person to an event in the event planner.
  * Guarantees: person is immutable and not null
  */
 public class Attendance {
+
+    private final Person attendee;
+    private boolean attended;
+
+    /**
+     * Person must be not be null
+     * @param attendee
+     */
+    public Attendance(Person attendee) {
+        Objects.requireNonNull(attendee);
+        this.attendee = attendee;
+        attended = false;
+    }
+
+    public Person getAttendee() {
+        return attendee;
+    }
+
+    public boolean hasAttended() {
+        return attended;
+    }
+
+    /**
+     * Sets the attendance for this attendee
+     */
+    public void setAttendance(boolean attended) {
+        this.attended = attended;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Attendance)) {
+            return false;
+        }
+
+        Attendance otherAttendance = (Attendance) other;
+        return otherAttendance.getAttendee().equals(this.getAttendee());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(attendee, attended);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(attendee.getName())
+                .append(" Attendance: ");
+                Boolean.toString(attended);
+        return builder.toString();
+    }
 }
