905bd8d13942e6357522e79b2e08d0098a16264d
william6364 <ganweiliang@gmail.com>
2018-04-01 13:16:16 +0800

    Added ToggleAttendanceCommandParser

---
+++ src/main/java/seedu/address/logic/parser/ToggleAttendanceCommandParser.java
@@ -0,0 +1,29 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.ToggleAttendanceCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new ToggleAttendanceCommand object
+ */
+public class ToggleAttendanceCommandParser implements Parser<ToggleAttendanceCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the ToggleAttendanceCommand
+     * and returns an ToggleAttendanceCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public ToggleAttendanceCommand parse(String args) throws ParseException {
+        try {
+            Index index = ParserUtil.parseIndex(args);
+            return new ToggleAttendanceCommand(index);
+        } catch (IllegalValueException ive) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, ToggleAttendanceCommand.MESSAGE_USAGE));
+        }
+    }
+}
