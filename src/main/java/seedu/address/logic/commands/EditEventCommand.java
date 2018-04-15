6204bc003f9473f0bdf886b9bab6127958841869
Bay Wei Heng <bayweiheng@gmail.com>
2018-04-15 15:40:19 +0800

    fix checkstyle

--- src/main/java/seedu/address/logic/commands/EditEventCommand.java
+++ src/main/java/seedu/address/logic/commands/EditEventCommand.java
@@ -3,7 +3,6 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_ATTENDEES;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_EVENTS;
 
 import java.util.Collections;
